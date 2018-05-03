5afd5d0cb6d51ba8236336c8e3a1b9f6cc48ac54
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-05-03 21:49:40 +0200

    last

--- Objects/Gate.py
+++ Objects/Gate.py
@@ -17,12 +17,12 @@ class Gate(Node):
             cylinderPerlin = PerlinMesh(cylinder)
             door_left = Node(children=[cube],transform=translate(5,0,0)@scale(10,2,0.7))
             scale_left = Node(children=[cylinderPerlin, door_left], transform=scale(0.1,height,0.1))
-            rotation_left = RotationControlNode( glfw.KEY_L, glfw.KEY_M,vec(0,1,0), speed=0.5, children=[scale_left])
+            rotation_left = RotationControlNode( glfw.KEY_L, glfw.KEY_P,vec(0,1,0), speed=0.5, children=[scale_left])
             node_left = Node(children=[rotation_left], transform=translate(-1,0,0))
 
             door_right = Node(children=[cube],transform=translate(-5,0,0)@scale(10,2,0.7))
             scale_right = Node(children=[cylinderPerlin, door_right], transform=scale(0.1,height,0.1))
-            rotation_right = RotationControlNode( glfw.KEY_L, glfw.KEY_M,vec(0,-1,0), speed=0.5, children=[scale_right])
+            rotation_right = RotationControlNode( glfw.KEY_L, glfw.KEY_P,vec(0,-1,0), speed=0.5, children=[scale_right])
             node_right = Node(children=[rotation_right], transform=translate(1,0,0))
 
             self.add(node_left, node_right)
